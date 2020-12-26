fc663fb8e4ee358873bf418455211f26950ac021
Joshua Galit <joshuaimalay@gmail.com>
2020-12-26 23:30:06 +0800

    Top navigation design sm and lg - DONE

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>
-    
-  </div>
+  <header>
+    <!-- Top navigation -->
+    <div class="bg-blue-600">
+      <div class="container mx-auto max-w-7xl px-4">
+        <div class="flex items-center justify-between flex-wrap py-4">
+          <div class="block sm:hidden">
+            <svg  class="w-8 h-8 text-white"
+                  stroke="currentColor" 
+                  xmlns:xlink="http://www.w3.org/1999/xlink" 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  viewBox="0 0 24 24" 
+                  preserveAspectRatio="xMidYMid meet">
+              <path xmlns="http://www.w3.org/2000/svg" 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    stroke-width="1.5" 
+                    d="M4 6h16M4 12h16M4 18h16">
+              </path>
+            </svg>
+          </div>
+          <div class="text-white text-2xl">cointbase</div>
+          <div class="flex-shrink-0">
+            <div class="focus:outline-none rounded-full block">
+              <div class="sm:flex sm:items-center">
+                <div>
+                  <img class="inline-block w-8 h-8 rounded-full" src="@/assets/avatar.jpg" alt="">
+                </div>
+                <div class="ml-1 hidden sm:flex sm:items-center">
+                  <span class="text-white font-medium text-sm">Joshua Galit</span>
+                  <div class="ml-1">
+                    <svg  class="w-4 h-4 block text-white opacity-50" 
+                          fill="currentColor" 
+                          xmlns:xlink="http://www.w3.org/1999/xlink" 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          viewBox="0 0 20 20" 
+                          preserveAspectRatio="xMidYMid meet">
+                      <path xmlns="http://www.w3.org/2000/svg" 
+                            fill-rule="evenodd" 
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" 
+                            clip-rule="evenodd">
+                      </path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+  </header>
 </template>
